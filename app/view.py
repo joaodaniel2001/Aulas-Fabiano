8418592dcd09c8a3f6c56ff504dd6ea76492ab58
joaodaniel2001 <joaodaniel2al@gmail.com>
2025-07-04 12:31:01 -0300

    04.07.2025

--- app/view.py
+++ app/view.py
@@ -1,7 +1,8 @@
 
 from app import app, db
-from flask import render_template, url_for, request
+from flask import render_template, url_for, request, redirect
 from app.forms import ContatoForm
+from app.models import Contato
 
 @app.route('/')
 def homepage():
@@ -22,5 +23,13 @@ def contato ():
     
     if form.validate_on_submit ():
         form.save ()
+        return redirect(url_for('homepage'))
     
     return render_template ('contato.html', context=context, form=form)
+
+@app.route('/contato/lista/')
+def contatoLista ():
+    dados = Contato.query.all()
+    print(dados)
+    context = {}
+    return render_template ('contatoLista.html', context=context)
