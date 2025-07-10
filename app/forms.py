880fdffa7736552aad3912558ef08e76859a8aaf
joaodaniel2001 <joaodaniel2al@gmail.com>
2025-07-10 10:27:52 -0300

    10.07.2025

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,34 @@
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 
-from app import db
-from app.models import Contato
+from app import db, bcrypt
+from app.models import Contato, User
+
+class UserForm (FlaskForm):
+    nome = StringField ('Nome', validators=[DataRequired()])
+    sobrenome = StringField ('Sobrenome', validators=[DataRequired()])
+    email = StringField ('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    confirmacao_senha = PasswordField('Senha', validators=[DataRequired(), EqualTo('senha')])
+    btnSubmit = SubmitField('Cadastrar')
+
+    def validade_email(self, email):
+        if User.query.filter(email=email.data).first():
+            return ValidationError('Usuário já cadastrado com esse E-mail!')
+
+    def save (self):
+        senha = bcrypt.generate_password_hash(self.senha.data.encode('utf-8'))
+        user = User (
+            nome = self.nome.data,
+            sobrenome = self.sobrenome.data,
+            email = self.email.data,
+            senha = senha
+        )
+        db.session.add(user)
+        db.session.commit()
+        return user
 
 class ContatoForm (FlaskForm):
     nome = StringField('Nome', validators = [DataRequired()])
